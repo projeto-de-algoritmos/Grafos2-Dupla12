d73423485b97e4e329fcda63954e06da419c3832
Iago Cabral <87732154+iagocabral@users.noreply.github.com>
2023-05-15 08:30:53 -0300

    Update ex1_NetworkTimeDelay.py

--- ex1_NetworkTimeDelay.py
+++ ex1_NetworkTimeDelay.py
@@ -1,20 +1,22 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        edges = collections.defaultdict(list) #lista de adjacencia
+        arrestas = collections.defaultdict(list) #lista de adjacencia
         for u, v, w in times:
-            edges[u].append((v,w)) # v = vizinho // w = peso
+            arrestas[u].append((v,w)) # v = vizinho // w = peso
         
         minHeap = [(0, k)] #startando a heap
-        visit = set() #monitorando todos os nos que visitamos
+        visitado = set() #monitorando todos os nos que visitamos
         tempo = 0 # resultado
+
+        #Logica de Dijkstra
         while minHeap:
             w1, n1 = heapq.heappop(minHeap)
-            if n1 in visit:
+            if n1 in visitado:
                 continue
-            visit.add(n1) # adcionar como visitiado
+            visitado.add(n1) # adcionar como visitiado
             tempo = max(tempo, w1) # increase tempo
-
-            for n2, w2 in edges[n1]:
-                if n2 not in visit:
+            for n2, w2 in arrestas[n1]:
+                if n2 not in visitado:
                     heapq.heappush(minHeap, (w1 + w2, n2))
-        return tempo if len(visit) == n else -1
+
+        return tempo if len(visitado) == n else -1
