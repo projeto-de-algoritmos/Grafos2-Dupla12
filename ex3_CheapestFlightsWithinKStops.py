c2aa36bd92d0a11911f0411673643e02a5bbfc0e
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2023-05-15 20:50:10 -0300

    Update ex3_CheapestFlightsWithinKStops.py
    Co-Authored-By: Iago Cabral <87732154+iagocabral@users.noreply.github.com>

--- ex3_CheapestFlightsWithinKStops.py
+++ ex3_CheapestFlightsWithinKStops.py
@@ -1,24 +1,22 @@
-import heapq
+from heapq import heappush, heappop
+from collections import defaultdict
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = {i: [] for i in range(n)}
         for s, d, p in flights:
-            graph[s].append((d, p))
-
-        heap = [(0, src, k)]
+            graph[s].append((d,p))
+        h = [(0, 0, src)]
+        visited = set()
         prices = {i: float('inf') for i in range(n)}
 
-        while heap:
-            price, city, conexao = heapq.heappop(heap)
+        while h:
+            price, city, conexao = heappop(h)
 
-            if city == dst:
-                return price
+            if conexao == dst: return price
 
-            if conexao >= 0:
-                for neighbor, cost in graph[city]:
-                    if price + cost < prices[neighbor]:
-                        prices[neighbor] = price + cost
-                        heapq.heappush(heap, (price + cost, neighbor, conexao - 1))
-
+            if (conexao, city) in visited or city > k: continue
+            visited.add((conexao, city))
+            for vizinho, custo in graph[conexao]:
+                heappush(h, (price+custo, city + 1, vizinho))
         return -1
